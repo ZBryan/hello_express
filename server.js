2487ae5563c0f69845988a2fe3723f7cf89755db
Zach Bryan <zachx1@gmail.com>
2014-05-06 15:21:05 -0700

    express demo

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+		http = require('http');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));	
+
+app.set('port', process.env.PORT || 3000);
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+	console.log('the server is running', app.get('port'))
+});
