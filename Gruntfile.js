d87d1f774e7ffdacc7ba0e981c26284d1c0d0c4d
Zach Bryan <zachx1@gmail.com>
2014-05-08 15:00:26 -0700

    casper

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports =function(grunt){
+
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-express-server');
+grunt.loadNpmTasks('grunt-casper');
+
+grunt.initConfig({
+	express: {
+		options: {
+
+		},
+		dev: {
+			options: {
+				script: 'server.js'
+			}
+		},
+		prod: {
+			options: {
+				script: 'server.js',
+				node_env: 'production'
+			}
+		},
+		test: {
+			options: {
+				script: 'server.js'
+			}
+		}
+	},
+	casper: {
+		acceptance : {
+			options : {
+				test : true,
+			},
+			files : {
+				'test/acceptance/casper-results.xml' : ['test/acceptance/*_test.js']
+			}
+		}
+	}
+});
+
+grunt.registerTask('server', ['jshint', 'express:dev']);
+grunt.registerTask('test', ['express:dev', 'casper']);
+grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ test/acceptance/home_page_test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+casper.test.begin('home page', 3, function suite (test){
+	casper.start('http://localhost:3000/', function (){
+		test.assertHttpStatus(200);
+	});
+
+	casper.then(function(){
+		test.assertTitle('Hello World Express', 'title is hellow world express');
+	});
+
+	casper.then(function(){
+		test.assertSelectorHasText('h1', 'Hello World');
+	});
+
+	casper.run(function(){
+		test.done();
+	});
+});
